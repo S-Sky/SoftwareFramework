a43deeb26d1a81b4c96fb758ae8c72795781a9be
[S-Sky] <[15809439371@163.com]>
2017-12-26 10:11:17 +0800

    框架一

--- app/src/main/java/com/sky/SoftwareFramework/activity/MainActivity.java
+++ app/src/main/java/com/sky/SoftwareFramework/activity/MainActivity.java
@@ -19,6 +19,10 @@ import java.util.List;
 import butterknife.BindView;
 import butterknife.ButterKnife;
 
+/**
+ * 框架一
+ */
+
 public class MainActivity extends FragmentActivity {
 
     @BindView(R.id.rg_main)

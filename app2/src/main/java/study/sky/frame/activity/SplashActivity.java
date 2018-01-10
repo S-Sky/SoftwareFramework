c504e190b709649b38e0671a682f6a1a2535c781
S-Sky <15809439371@163.com>
2018-01-10 18:49:49 +0800

    多渠道打包

--- app2/src/main/java/study/sky/frame/activity/SplashActivity.java
+++ app2/src/main/java/study/sky/frame/activity/SplashActivity.java
@@ -6,8 +6,10 @@ import android.os.Handler;
 import android.os.Message;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.Toast;
 
 import study.sky.frame.R;
+import study.sky.frame.utils.ChannelUtil;
 
 /**
  * Created by Administrator on 2017/12/14 0014.
@@ -20,6 +22,8 @@ public class SplashActivity extends AppCompatActivity {
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+        String channel = ChannelUtil.getChannel(this,"defaultChannel");
+        Toast.makeText(this, "channel==" + channel, Toast.LENGTH_SHORT).show();
         init();
     }
 
